782c4735e5ce107f5cbc37639ef14b9332f80883
Sebastian <sebastian.koller7@gmx.at>
2024-02-26 17:35:44 +0100

    Initial Commit

---
+++ SwiftUI Game/SwiftUI_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GameApp.swift
+//  SwiftUI Game
+//
+//  Created by Sebastian Koller on 26.02.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
